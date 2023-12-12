4b2bbcd5ea1a938456337c6de6206a6ce979a10b
Hans Pagel <mail@hanspagel.com>
2023-12-12 12:55:16 +0100

    chore: don’t use alias on build

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,15 +3,23 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      // Resolve the uncompiled source code for all @scalar packages.
-      {
-        find: /^@scalar\/([^/]+)$/,
-        replacement: path.resolve(__dirname, '../packages/$1/src/index.ts'),
-      },
-    ],
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    resolve:
+      command === 'serve'
+        ? {
+            alias: [
+              // Resolve the uncompiled source code for all @scalar packages.
+              {
+                find: /^@scalar\/([^/]+)$/,
+                replacement: path.resolve(
+                  __dirname,
+                  '../packages/$1/src/index.ts'
+                ),
+              },
+            ],
+          }
+        : {},
+  }
 })
