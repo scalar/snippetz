ba9917b49c792adad33e0cd639aba87a4380cea7
marclave <marc@scalar.com>
2024-04-02 11:34:47 -0700

    fix: tests

--- packages/snippetz/src/snippetz.test.ts
+++ packages/snippetz/src/snippetz.test.ts
@@ -17,7 +17,7 @@ const { statusCode, body } = await request('https://example.com')`)
     expect(targets).toStrictEqual(['node', 'js'])
 
     const clients = snippetz().clients()
-    expect(clients).toStrictEqual(['undici', 'fetch', 'fetch'])
+    expect(clients).toStrictEqual(['undici', 'fetch', 'fetch', 'ofetch'])
   })
 })
 
